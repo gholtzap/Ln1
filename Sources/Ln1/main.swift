3946cb27027fe74fb21d7550c57c80f646e102da
Gavin <gholtzap@asu.edu>
2026-05-10 21:35:46 -0400

    Move task memory models

--- Sources/Ln1/main.swift
+++ Sources/Ln1/main.swift
@@ -973,32 +973,6 @@ struct IdentityVerification: Codable {
     let confidenceAccepted: Bool?
 }
 
-struct TaskMemoryEvent: Codable {
-    let id: String
-    let timestamp: String
-    let taskID: String
-    let kind: String
-    let status: String?
-    let title: String?
-    let summary: String?
-    let summaryLength: Int?
-    let summaryDigest: String?
-    let sensitivity: String
-    let relatedAuditID: String?
-}
-
-struct TaskMemoryResult: Codable {
-    let path: String
-    let taskID: String
-    let status: String?
-    let title: String?
-    let startedAt: String?
-    let updatedAt: String?
-    let eventCount: Int
-    let limit: Int
-    let events: [TaskMemoryEvent]
-}
-
 let args = Array(CommandLine.arguments.dropFirst())
 
 do {
